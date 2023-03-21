72ac4f9f9140afa70691c8a48906a357fb136d01
Nonbell18 <nonbel18go@gmail.com>
2023-03-21 17:52:27 +0100

    Custom blocks added!

--- src/main/java/com/example/simplytech/block/ModBlocks.java
+++ src/main/java/com/example/simplytech/block/ModBlocks.java
@@ -23,29 +23,33 @@ public class ModBlocks {
     public static final DeferredRegister<Block> BLOCKS =
             DeferredRegister.create(ForgeRegistries.BLOCKS, SimplyTech.MOD_ID);
 
-    public static final RegistryObject<Block> CITRINE_BLOCK = registerBlock("citrine_block",
+
+    public static final RegistryObject<Block> CITRINE_BLOCK = registerblock("citrine_block",
             () -> new Block(BlockBehaviour.Properties.of(Material.STONE)
                     .strength(6f).requiresCorrectToolForDrops()), ModCreativeModeTab.TAB_SIMPLY_TECH);
 
-    public static final RegistryObject<Block> CITRINE_ORE = registerBlock("citrine_ore",
+
+
+    public static final RegistryObject<Block> CITRINE_ORE = registerblock("citrine_ore",
             () -> new DropExperienceBlock(BlockBehaviour.Properties.of(Material.STONE)
                     .strength(6f).requiresCorrectToolForDrops(), UniformInt.of(3, 7)), ModCreativeModeTab.TAB_SIMPLY_TECH);
 
-    public static final RegistryObject<Block> DEEPSLATE_CITRINE_ORE = registerBlock("deepslate_citrine_ore",
+
+
+    public static final RegistryObject<Block> DEEPSLATE_CITRINE_ORE = registerblock("deepslate_citrine_ore",
             () -> new DropExperienceBlock(BlockBehaviour.Properties.of(Material.STONE)
                     .strength(6f).requiresCorrectToolForDrops(), UniformInt.of(3, 7)), ModCreativeModeTab.TAB_SIMPLY_TECH);
 
-        private static <T extends Block> RegistryObject<T> registerBlock(String name, Supplier<T> block, CreativeModeTab tab) {
-            RegistryObject<T> toReturn = BLOCKS.register(name, block);
-            RegisterBlockItem(name, toReturn, tab);
+    private static <T extends Block> RegistryObject<T> registerblock(String name, Supplier<T> block, CreativeModeTab tab) {
+        RegistryObject<T> toReturn = BLOCKS.register(name, block);
+        registerBlockItem(name, toReturn, tab);
+        return toReturn;
+    }
 
-            return toReturn;
-        }
-
-        private static <T extends Block> RegistryObject<Item> RegisterBlockItem(String name, RegistryObject<T> block,
-                                                                                CreativeModeTab tab) {
-            return Moditems.ITEMS.register(name, () -> new BlockItem(block.get(), new Item.Properties().tab(tab)));
-        }
+    private static <T extends Block> RegistryObject<Item> registerBlockItem(String name, RegistryObject<T> block,
+                                                                            CreativeModeTab tab) {
+        return Moditems.ITEMS.register(name, () -> new BlockItem(block.get(), new Item.Properties().tab(tab)));
+    }
 
 
     public static void register(IEventBus eventBus) {
